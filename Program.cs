e6f3bbf1d3ee5796ef11365421d454f4375a2e68
Adva <advaoren2015@gmail.com>
2024-08-23 00:25:12 +0300

    build

--- Program.cs
+++ Program.cs
@@ -1,6 +1,57 @@
-using TaskManagementApi.Models; 
+// using TaskManagementApi.Models; 
+// using TaskManagementApi.Repositories;
+// using Microsoft.Extensions.Options;
+
+// var builder = WebApplication.CreateBuilder(args);
+
+// // Allowed origins
+// string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+// // Configure CORS
+// builder.Services.AddCors(options =>
+// {
+//     options.AddPolicy(name: MyAllowSpecificOrigins,
+//         policy  =>
+//         {
+//             policy.WithOrigins("http://localhost:3000")
+//                   .AllowAnyHeader()
+//                   .AllowAnyMethod();
+//         });
+// });
+// // Add services to the container.
+// builder.Services.Configure<DatabaseSettings>(
+//     builder.Configuration.GetSection("DatabaseSettings"));
+
+// // Register the ITaskRepository service with its implementation
+// builder.Services.AddSingleton<ITaskRepository, TaskRepository>();
+
+// builder.Services.AddControllers();
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
+
+// var app = builder.Build();
+// // Use CORS
+// app.UseCors(MyAllowSpecificOrigins);
+
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+// app.UseHttpsRedirection();
+
+// app.UseAuthorization();
+
+// app.MapControllers();
+
+// app.Run();
+using TaskManagementApi.Models;
 using TaskManagementApi.Repositories;
 using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,13 +62,14 @@ string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: MyAllowSpecificOrigins,
-        policy  =>
+        policy =>
         {
             policy.WithOrigins("http://localhost:3000")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
 });
+
 // Add services to the container.
 builder.Services.Configure<DatabaseSettings>(
     builder.Configuration.GetSection("DatabaseSettings"));
@@ -30,6 +82,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 // Use CORS
 app.UseCors(MyAllowSpecificOrigins);
 
@@ -41,8 +94,18 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// Serve static files from wwwroot
+app.UseStaticFiles();
+
+// Use routing
+app.UseRouting();
+
 app.UseAuthorization();
 
+// Map API controllers
 app.MapControllers();
+
+// Serve the React app for non-API routes
+app.MapFallbackToFile("/ClientApp/index.html");
 
 app.Run();
